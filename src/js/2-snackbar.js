2568e08b4730d9ef733c80d0394ac971ccee381a
NastyaTron <gabylia445@gmail.com>
2024-05-07 12:37:06 +0300

    07.05

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,9 +1,13 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 const formEl = document.querySelector('.form');
 const inputEl = document.querySelector('.js-input');
 const clickFulfilled = document.querySelector('.js-click-ful');
 const clickRejected = document.querySelector('.js-click-rej');
 
 formEl.addEventListener('submit', onFormSubmit);
+
 function onFormSubmit(event) {
   event.preventDefault();
   const delay = inputEl.valueAsNumber;
@@ -18,7 +22,35 @@ function onFormSubmit(event) {
   });
   promise
     .then(delay => {
-      console.log(`✅ Fulfilled promise in ${delay}ms`);
+      iziToast.error({
+        timeout: false,
+        position: 'topRight',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        messageColor: '#fff',
+        backgroundColor: '#5cc285',
+        close: false,
+        closeOnClick: true,
+        icon: false,
+      });
+      removeForm();
     })
-    .catch(delay => console.log(`❌ Rejected promise in ${delay}ms`));
+    .catch(delay => {
+      iziToast.error({
+        timeout: false,
+        position: 'topRight',
+        message: `❌ Rejected promise in ${delay}ms`,
+        messageColor: '#fff',
+        backgroundColor: '#d37a7a',
+        close: false,
+        closeOnClick: true,
+        icon: false,
+      });
+      removeForm();
+    });
+}
+
+function removeForm() {
+  inputEl.value = '';
+  clickFulfilled.checked = false;
+  clickRejected.checked = false;
 }
