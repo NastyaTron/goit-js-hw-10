41858724072063633a0d339c7a324145c4a80f8f
NastyaTron <gabylia445@gmail.com>
2024-05-07 11:31:04 +0300

    Update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,12 +1,12 @@
-const form = document.querySelector('.form');
-const input = document.querySelector('.js-input');
+const formEl = document.querySelector('.form');
+const inputEl = document.querySelector('.js-input');
 const clickFulfilled = document.querySelector('.js-click-ful');
 const clickRejected = document.querySelector('.js-click-rej');
 
-form.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('submit', onFormSubmit);
 function onFormSubmit(event) {
   event.preventDefault();
-  const delay = input.valueAsNumber;
+  const delay = inputEl.valueAsNumber;
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       if (clickFulfilled.checked) {
@@ -15,7 +15,6 @@ function onFormSubmit(event) {
         reject(delay);
       }
     }, delay);
-    console.log(promise);
   });
   promise
     .then(delay => {
@@ -23,5 +22,3 @@ function onFormSubmit(event) {
     })
     .catch(delay => console.log(`❌ Rejected promise in ${delay}ms`));
 }
-
-console.log('mango');
